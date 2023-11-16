da2fb70feba785235702a9043e5dc3f0ac58cd88
haan <hyj783920@gmail.com>
2023-11-17 03:00:19 +0900

    3:00

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h4>뭘 봐</h4>
+      <div className="mainImg">
+        <img
+          className="logo"
+          src="https://udwns310.github.io/dongcar/DongCar.png"
+        />
+      </div>
+      <div className="main">
+        <h4 className="ment">저렴하게 이동하세요</h4>
+      </div>
+
+      <div className="d-grid gap-2">
+        <Button className="startbtn" variant="primary" size="lg">
+          시작하기
+        </Button>
+      </div>
     </div>
   );
 }
