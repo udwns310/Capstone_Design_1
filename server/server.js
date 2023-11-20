84a15db3ba8d64fd0f48f8ab279920cfaae46204
flskhhdf <flskhhdf@naver.com>
2023-11-20 21:28:16 +0900

    merge

--- server/server.js
+++ server/server.js
@@ -20,3 +20,15 @@ app.post('/text', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const autoHyphen = (target) => {
+  target.value = target.value
+    .replace(/[^0-9]/g, '')
+    .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
+ }
+ 
+ const autoHyphen2 = (target) => {
+  target.value = target.value
+    .replace(/[^0-9]/g, '')
+   .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "");
+ }
