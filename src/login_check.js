e900615ba280a430f251880118b3d65968e64c04
udwns310 <101700659+udwns310@users.noreply.github.com>
2023-11-20 15:52:18 +0900

    test

--- src/login_check.js
+++ src/login_check.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const session = require('express-session')
-const path = require('path');
-const app = express()
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const app = express();
 
-const db = require('../lib/db');
+const db = require("../lib/db");
 const bodyParser = require("body-parser");
-
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LoginForm from "../components/loginform";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Login = () => {
+  let [fade, setFade] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFade("end");
+    }, 1000);
+    return () => {
+      setFade("");
+    };
+  }, []);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -36,23 +47,25 @@ const Login = () => {
   };
 
   return (
-    <Row>
-      <Col xs={1} md={3}></Col>
-      <Col xs={10} md={6}>
-        <Card body style={{ marginTop: "1rem", borderRadius: "10px" }}>
-          <h3>DongCar</h3>
-          <h5>Taxi Sharing Service</h5>
-          <LoginForm
-            email={email}
-            password={password}
-            handleEmail={handleEmail}
-            handlePwd={handlePwd}
-            handleSubmit={handleSubmit}
-          />
-        </Card>
-      </Col>
-      <Col xs={1} md={3}></Col>
-    </Row>
+    <div className={"login start " + fade}>
+      <Row>
+        <Col xs={1} md={3}></Col>
+        <Col xs={10} md={6}>
+          <Card body style={{ marginTop: "1rem", borderRadius: "10px" }}>
+            <h3>DongCar</h3>
+            <h5>Taxi Sharing Service</h5>
+            <LoginForm
+              email={email}
+              password={password}
+              handleEmail={handleEmail}
+              handlePwd={handlePwd}
+              handleSubmit={handleSubmit}
+            />
+          </Card>
+        </Col>
+        <Col xs={1} md={3}></Col>
+      </Row>
+    </div>
   );
 };
 export default Login;
