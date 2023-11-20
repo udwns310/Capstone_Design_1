84185eb3722dc89fb72a3a69ef36733fc536f780
udwns310 <101700659+udwns310@users.noreply.github.com>
2023-11-20 23:19:26 +0900

    Delete src/login_check.js

--- src/login_check.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const session = require("express-session");
-const path = require("path");
-const app = express();
-
-const db = require("../lib/db");
-const bodyParser = require("body-parser");
