a3331e295336757b894c8f19ba307b4e3cfe8815
KimHS17 <skes01149@naver.com>
2023-11-21 15:50:06 +0900

    Update dbquery.js

--- server/dbquery.js
+++ server/dbquery.js
@@ -1,32 +1,5 @@
 const db = require("../lib/db");
 
-exports.register = function (req, res) {
-  const post = req.body;
-  const hasEmptyValue = Object.values(post).some(value => value.trim() === '');
-  if (hasEmptyValue) {
-    res.json({ status: 'error', message: 'Login successful' })
-  }
-  else {
-    db.query(`SELECT * FROM profile where email = ? OR stdId = ?`,
-      [post.email, post.stdId], function (err, rows) {
-        if (err) throw err;
-        if (rows.length == 0) {
-          db.query(`INSERT INTO profile VALUES (?, ?, ?, ?, ?, ?)`,
-            [post.email, post.password, post.name, post.gender, post.phoneNum, post.stdId],
-            function (error, result) {
-              if (error)
-                throw error;
-              console.log("good!");
-            }
-          )
-        }
-        else {
-          console.log('not good');
-        }
-      })
-  }
-}
-
 exports.login = function (req, res) {
   const post = req.body;
   db.query('SELECT * FROM profile where email = ? AND password = ?',
