e888d92f9811607011baed1dc595a58347c96505
KimHS17 <skes01149@naver.com>
2023-11-22 00:09:25 +0900

    회원가입 중복확인 분리

--- server/dbquery.js
+++ server/dbquery.js
@@ -26,25 +26,32 @@ exports.register = function (req, res) {
     res.json({ status: 'error', message: 'Register failed' })
   }
   else {
-    db.query(`SELECT * FROM profile where email = ? OR stdId = ?`,
-      [post.email, post.stdId], function (err, rows) {
-        if (err) throw err;
-        if (rows.length == 0) {
-          db.query(`INSERT INTO profile VALUES (?, ?, ?, ?, ?, ?, NULL)`,
-            [post.email, post.password, post.name, post.gender, post.phoneNum, post.stdId],
-            function (error, result) {
-              if (error) {
-                res.status(500).json({ message: 'Internal Server Error' });
-                throw error;
+    db.query(`SELECT * FROM profile where email = ?`, // email이 중복되는지 검사
+      [post.email], function (Eerr, Erows) {
+        if (Eerr) throw Eerr;
+        if (Erows.length == 0) { // email이 중복되지 않는다면
+          db.query(`SELECT * FROM profile where stdId = ?`, // stdId가 중복되는지 검사
+            [post.stdId], function (Serr, Srows) {
+              if (Serr) throw Serr;
+              if (Srows.length == 0) { // stdId가 중복되지 않는다면
+                db.query(`INSERT INTO profile VALUES (?, ?, ?, ?, ?, ?, NULL)`, // DB에 데이터 삽입
+                  [post.email, post.password, post.name, post.gender, post.phoneNum, post.stdId],
+                  function (error, result) {
+                    if (error) {
+                      res.status(500).json({ message: 'Internal Server Error' });
+                      throw error;
+                    }
+                    res.json({ status: 'success', message: 'Register successful' }) // 회원가입 성공
+                  }
+                )
               }
-              console.log("good!");
-              res.json({ status: 'success', message: 'Register successful' })
-            }
-          )
+              else {
+                res.json({ status: 'stdIdDuplicate', message: 'Register failed' }) // 학번 중복
+              }
+            })
         }
         else {
-          res.json({ status: 'duplicate', message: 'Register failed' })
-          console.log('not good');
+          res.json({ status: 'emailDuplicate', message: 'Register failed' }) // 이메일 중복
         }
       })
   }
