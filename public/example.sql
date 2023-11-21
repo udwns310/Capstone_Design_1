fd0633f43901c417e5e463ddc7a12c0e2fb22f26
KimHS17 <skes01149@naver.com>
2023-11-22 02:36:19 +0900

    Update example.sql

--- public/example.sql
+++ public/example.sql
@@ -4,13 +4,13 @@
 
 CREATE TABLE `profile` (
   `email` varchar(30) NOT NULL,
-  `password` varchar(80) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `name` varchar(20) NOT NULL,
   `gender` int(1) NOT NULL,
   `phoneNum` varchar(20) NOT NULL,
-  `stdId` int(8) NOT NULL,
+  `stdId` varchar(10) NOT NULL,
   `nickname` varchar(20),
-  `salt` varchar(80),
+  `salt` varchar(200),
   PRIMARY KEY (`stdId`)
 );
 
@@ -18,4 +18,6 @@ CREATE TABLE `profile` (
 -- Dumping data for table `profile`
 --
 
-INSERT INTO `profile` VALUES ('test@test.com','Xsc+HAlHasoKQy/Ct5EdOU5gFnaOT3PUdPFc1Qn6g9pYby1emDhvp83oWSFlxr9I5x4YNNkVpMpX3FUJzeRYdA==','test',0,'010-1234-5678', '20231111', 'testNick', "OlHm8Do1/0Oj8mrtpgVIyO6XTl0X0V6JQtUlzyONNx3SyY32B648B95t6UxS8ki+Tax7BkidTMp66tlpxbtGYA==");
+INSERT INTO `profile` VALUES ('test@test.com','Xsc+HAlHasoKQy/Ct5EdOU5gFnaOT3PUdPFc1Qn6g9pYby1emDhvp83oWSFlxr9I5x4YNNkVpMpX3FUJzeRYdA==','test',0,'010-1234-5678', '20231111', 'testNick', 'OlHm8Do1/0Oj8mrtpgVIyO6XTl0X0V6JQtUlzyONNx3SyY32B648B95t6UxS8ki+Tax7BkidTMp66tlpxbtGYA==');
+
+--password = qwer
