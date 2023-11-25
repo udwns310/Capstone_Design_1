71e5412ac9d94e6a7ec3e3fcbad1bc68a5e92261
KimHS17 <skes01149@naver.com>
2023-11-26 00:49:01 +0900

    채팅목록 수정

--- src/pages/chatlist.js
+++ src/pages/chatlist.js
@@ -16,41 +16,32 @@ function Chatlist(props) {
 
   return (
     <div className={"Chatlist start " + fade}>
-      <ListGroup as="ol" className="list">
-        <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-        >
-            <div className="ms-2 me-auto">
-            <div className="fw-bold">Subheading</div>
-            </div>
-            <Badge bg="primary" pill>
-              3 / 4
-            </Badge>
-        </ListGroup.Item>
-        <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-        >
-            <div className="ms-2 me-auto">
-            <div className="fw-bold">Subheading</div>
-            </div>
-            <Badge bg="primary" pill>
-              1 / 4
-            </Badge>
-        </ListGroup.Item>
-        <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-        >
-            <div className="ms-2 me-auto">
-            <div className="fw-bold">Subheading</div>
-            </div>
-            <Badge bg="primary" pill>
-              2 / 4
-            </Badge>
-        </ListGroup.Item>
-      </ListGroup>
+      <div className="list">
+        <div className="list_component">동의대역 - 자대로터리<div className="list_num">3 / 4</div></div>
+        <div className="list_component">가야 1치안 - 수덕전<div className="list_num">1 / 4</div></div>
+        <div className="list_component">가야 1치안 - 행복기숙사<div className="list_num">2 / 4</div></div>
+        <div className="list_component">동의대역 - 본관<div className="list_num">1 / 4</div></div>
+        <div className="list_component">동의대역 - 자대로터리<div className="list_num">3 / 4</div></div>
+        <div className="list_component">가야 1치안 - 수덕전<div className="list_num">1 / 4</div></div>
+        <div className="list_component">가야 1치안 - 행복기숙사<div className="list_num">2 / 4</div></div>
+        <div className="list_component">동의대역 - 본관<div className="list_num">1 / 4</div></div>
+        <div className="list_component">동의대역 - 자대로터리<div className="list_num">3 / 4</div></div>
+        <div className="list_component">가야 1치안 - 수덕전<div className="list_num">1 / 4</div></div>
+        <div className="list_component">가야 1치안 - 행복기숙사<div className="list_num">2 / 4</div></div>
+        <div className="list_component">동의대역 - 본관<div className="list_num">1 / 4</div></div>
+        <div className="list_component">동의대역 - 자대로터리<div className="list_num">3 / 4</div></div>
+        <div className="list_component">가야 1치안 - 수덕전<div className="list_num">1 / 4</div></div>
+        <div className="list_component">가야 1치안 - 행복기숙사<div className="list_num">2 / 4</div></div>
+        <div className="list_component">동의대역 - 본관<div className="list_num">1 / 4</div></div>
+        <div className="list_component">동의대역 - 자대로터리<div className="list_num">3 / 4</div></div>
+        <div className="list_component">가야 1치안 - 수덕전<div className="list_num">1 / 4</div></div>
+        <div className="list_component">가야 1치안 - 행복기숙사<div className="list_num">2 / 4</div></div>
+        <div className="list_component">동의대역 - 본관<div className="list_num">1 / 4</div></div>
+        <div className="list_component">동의대역 - 자대로터리<div className="list_num">3 / 4</div></div>
+        <div className="list_component">가야 1치안 - 수덕전<div className="list_num">1 / 4</div></div>
+        <div className="list_component">가야 1치안 - 행복기숙사<div className="list_num">2 / 4</div></div>
+        <div className="list_component">동의대역 - 본관<div className="list_num">1 / 4</div></div>
+      </div>
     </div>
   );
 }
