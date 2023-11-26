a70dd94cafe998bc720d6c517b764a7c4ccc3fcc
udwns310 <101700659+udwns310@users.noreply.github.com>
2023-11-27 02:49:47 +0900

    Update main.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,51 +1,69 @@
-import React from "react";
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Map from "../components/map.js";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import Chatlist from "./chatlist.js";
+import Map from '../components/map.js';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Chatlist from './chatlist.js';
 
 function Main(props) {
-  return (
-    <div style={{ height: '100vh' }}>
-      <Routes>
-        <Route path='/' element={
-          <div style={{ height: '94vh' }}>
-          <Map />
-        </div>
-        }>
-        </Route>
-        <Route
-          path="/chatlist"
-          element={
-            <div style={{ height: '94vh'}}>
-              <Chatlist />
+    return (
+        <div style={{ height: '100vh' }}>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div style={{ height: '94vh' }}>
+                            <Map />
+                        </div>
+                    }
+                ></Route>
+                <Route
+                    path="/chatlist"
+                    element={
+                        <div style={{ height: '94vh' }}>
+                            <Chatlist />
+                        </div>
+                    }
+                ></Route>
+            </Routes>
+            <div style={{ height: '6vh' }}>
+                <ButtonGroupContainer />
             </div>
-          }
-        ></Route>
-      </Routes>
-      <div style={{ height: '6vh' }}>
-        <ButtonGroupContainer />
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 function ButtonGroupContainer() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleListButtonClick = (path) => {
-    navigate(path);
-  };
+    const handleListButtonClick = (path) => {
+        navigate(path);
+    };
 
-  return (
-    <ButtonGroup aria-label="Basic example" style={{ width: '100%', height: '100%' }}>
-      <Button variant="warning" style={{ width: '25%', borderRadius: '0' }} onClick={() => handleListButtonClick('/main')}>홈</Button>
-      <Button variant="warning" style={{ width: '25%', borderRadius: '0' }} onClick={() => handleListButtonClick('/main/chatlist')}>목록</Button>
-      <Button variant="warning" style={{ width: '25%', borderRadius: '0' }}>MY채팅</Button>
-      <Button variant="warning" style={{ width: '25%', borderRadius: '0' }}>계정관리</Button>
-    </ButtonGroup>
-  );
+    return (
+        <ButtonGroup aria-label="Basic example" style={{ width: '100%', height: '100%' }}>
+            <Button
+                variant="warning"
+                style={{ width: '25%', borderRadius: '0' }}
+                onClick={() => handleListButtonClick('/main')}
+            >
+                홈
+            </Button>
+            <Button
+                variant="warning"
+                style={{ width: '25%', borderRadius: '0' }}
+                onClick={() => handleListButtonClick('/main/chatlist')}
+            >
+                목록
+            </Button>
+            <Button variant="warning" style={{ width: '25%', borderRadius: '0' }}>
+                MY채팅
+            </Button>
+            <Button variant="warning" style={{ width: '25%', borderRadius: '0' }}>
+                계정관리
+            </Button>
+        </ButtonGroup>
+    );
 }
 
 export default Main;
