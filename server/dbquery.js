a0c177c74597ea92057b28cd508e47aee4b86c35
KimHS17 <skes01149@naver.com>
2023-12-01 23:42:38 +0900

    Update dpquery.js

--- server/dbquery.js
+++ server/dbquery.js
@@ -30,7 +30,6 @@ const verifyPassword = async (password, userSalt, userPassword) => { // password
 };
 
 exports.login = function (req, res, callback) {
-<<<<<<< Updated upstream
     const post = req.body;
     db.query(`SELECT password, salt FROM profile where email = ?`,
         [post.email],
@@ -89,67 +88,6 @@ exports.register = function (req, res) {
             }
         })
     }
-=======
-  const post = req.body;
-  db.query(`SELECT password, salt FROM profile where email = ?`,
-      [post.email],
-      async function (error, result) {
-        if (error) {
-            res.status(500).json({ message: 'Internal Server Error' });
-            return;
-        }
-        if (result.length > 0) {
-            const verified = await verifyPassword(post.password, result[0].salt, result[0].password); // password 검증
-            if (verified) {
-              callback({ status: 'success', message: 'Login successful' });
-            } else {
-              callback({ status: 'error', message: 'Login failed' });
-            }
-        } else {
-            callback({ status: 'error', message: 'Login failed' });
-        }
-      }
-  )
-}
-
-exports.register = function (req, res) {
-  const post = req.body;
-  const hasEmptyValue = Object.values(post).some(value => value.trim() === '');
-  if (hasEmptyValue) {
-    res.json({ status: 'error', message: 'Register failed' })
-  }
-  else {
-    db.query(`SELECT * FROM profile where email = ?`, // email이 중복되는지 검사
-      [post.email], function (Eerr, Erows) {
-        if (Eerr) throw Eerr;
-        if (Erows.length == 0) { // email이 중복되지 않는다면
-          db.query(`SELECT * FROM profile where stdId = ?`, // stdId가 중복되는지 검사
-            [post.stdId], async function (Serr, Srows) {
-              if (Serr) throw Serr;
-              if (Srows.length == 0) { // stdId가 중복되지 않는다면
-                const { password, salt } = await createHashedPassword(post.password); // password 암호화
-                db.query(`INSERT INTO profile VALUES (?, ?, ?, ?, ?, ?, NULL, ?)`, // DB에 데이터 삽입
-                  [post.email, password, post.name, post.gender, post.phoneNum, post.stdId, salt],
-                  function (error, result) {
-                    if (error) {
-                      res.status(500).json({ message: 'Internal Server Error' });
-                      throw error;
-                    }
-                    res.json({ status: 'success', message: 'Register successful' }) // 회원가입 성공
-                  }
-                )
-              }
-              else {
-                res.json({ status: 'stdIdDuplicate', message: 'Register failed' }) // 학번 중복
-              }
-            })
-        }
-        else {
-          res.json({ status: 'emailDuplicate', message: 'Register failed' }) // 이메일 중복
-        }
-      })
-  }
->>>>>>> Stashed changes
 }
 
 exports.nickname = function (req, res) {
@@ -176,13 +114,7 @@ exports.nickname = function (req, res) {
 };
 
 exports.chatlist = function (req, res, callback) {
-<<<<<<< Updated upstream
     db.query(`SELECT *, date_format(date, '%m/%d %H:%i') as formatDate FROM chatlist ORDER BY date`, function(err, result) {
         callback({ data: result });
     })
-=======
-  db.query(`SELECT *, date_format(date, '%m/%d %H:%i') as formatDate FROM chatlist ORDER BY date`, function(err, result) {
-    callback({ data: result });
-  })
->>>>>>> Stashed changes
 }
