3e11b83e8ce4bfb1f026e7bd8b1235d376898f18
KimHS17 <skes01149@naver.com>
2023-12-12 02:44:18 +0900

    채팅 중복 전송 수정

--- src/pages/chatRoom.js
+++ src/pages/chatRoom.js
@@ -7,17 +7,12 @@ const ChatRoom = () => {
   const [newMessage, setNewMessage] = useState('');
   const location = useLocation();
   const roomId = location.state?.roomId;
-  const [clientSocketId, setSocketId] = useState('');
-  const [isConnect, setIsConnect] = useState(false);
 
   useEffect(() => {
     const socket = io('http://localhost:3002/chat');
 
     socket.on('connect', () => {
-      console.log("connect room " + roomId);
       socket.emit('join', roomId);
-      setSocketId(socket.id);
-      setIsConnect(true);
     });
 
     socket.on('serverSendMessage', (message) => {
@@ -25,6 +20,7 @@ const ChatRoom = () => {
         setMessages((prevMessages) => [...prevMessages, message]);
         console.log("다른 놈이 보낸 채팅임");
       }
+      setNewMessage(''); // 입력 필드 초기화
     });
 
     return () => {
@@ -37,12 +33,11 @@ const ChatRoom = () => {
     // 클라이언트가 현재 속한 방에 메시지를 보냄
     socket.on('connect', () => {
       // console.log(socketId);
-      socket.emit('clientSendMessage', { clientSocketId, roomId, message: newMessage });
+      socket.emit('clientSendMessage', { roomId, message: newMessage });
     })
     // 입력한 메시지를 현재 메시지 목록에 추가
     setMessages((prevMessages) => [...prevMessages, newMessage]);
     console.log("내가 보낸 채팅임");
-    setNewMessage(''); // 입력 필드 초기화
   };
 
 
